a38fbd79c888824a6f3f6c631dcfc55c3adf93fd
jxd134 <jinxd134@foxmail.com>
2015-10-06 20:49:32 +0800

    Remove Element solution.

---
+++ leetcodetest/Remove Element/test.cpp
@@ -0,0 +1,22 @@
+#include"../leetcodetest/head.h"
+
+class Solution
+{
+public:
+	int removeElement(vector<int>& nums, int val)
+	{
+		size_t len = 0;
+
+		for (size_t i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] != val)
+				nums[len++] = nums[i];
+		}
+		return len;
+	}
+};
+
+int main()
+{
+	return 0;
+}
