04bfa7c0fe62945f950625a3fb2f986093df9132
jxd134 <jinxd134@foxmail.com>
2015-10-06 21:37:57 +0800

    Rectangle Area solution.

---
+++ leetcodetest/Rectangle Area/test.cpp
@@ -0,0 +1,24 @@
+#include"../leetcodetest/head.h"
+
+class Solution
+{
+public:
+	int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+	{
+		int result = (C - A)*(D - B) + (G - E)*(H - F);
+		if (C<E || D<F || A>G || B>H)
+			return result;
+
+		int top = min(D, H);
+		int bottom = max(B, F);
+		int left = max(A, E);
+		int right = min(C, G);
+
+		return result - (right - left)*(top - bottom);
+	}
+};
+
+int main()
+{
+	return 0;
+}
