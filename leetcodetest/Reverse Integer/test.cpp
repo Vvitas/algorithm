f6e1f6f484e30eed8ca25d07d5bf36b7a26b26b9
jxd134 <jinxd134@foxmail.com>
2015-10-06 22:23:46 +0800

    Reverse Integer solution

---
+++ leetcodetest/Reverse Integer/test.cpp
@@ -0,0 +1,38 @@
+#include"../leetcodetest/head.h"
+
+class Solution
+{
+public:
+	int reverse(int x)
+	{
+		queue<int> data;
+		bool sign = x < 0 ? true : false;
+
+		x *= (x < 0) ? -1 : 1;
+		while (x)
+		{
+			data.push(x % 10);
+			x = x / 10;
+		}
+
+		long long result = 0;
+		while (!data.empty())
+		{
+			result = result * 10 + data.front();
+			data.pop();
+
+			if (result > INT_MAX) return 0;
+		}
+
+		result *= sign ? -1 : 1;
+
+		if (result<INT_MIN || result>INT_MAX)
+			return 0;
+		else return result;
+	}
+};
+
+int main()
+{
+	return 0;
+}
