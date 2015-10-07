67c46823a6c4c1d1955c6f3d1e21c21e7747dfd9
jxd134 <jinxd134@foxmail.com>
2015-10-07 16:05:25 +0800

    Sort Colors solution.

---
+++ leetcodetest/Sort Colors/test.cpp
@@ -0,0 +1,31 @@
+#include"../leetcodetest/head.h"
+
+class Solution
+{
+public:
+	void sortColors(vector<int>& nums)
+	{
+		unsigned int i, j, numofcolor[3] = { 0 };
+
+		for (i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] == 0)
+				++numofcolor[0];
+			else if (nums[i] == 1)
+				++numofcolor[1];
+			else ++numofcolor[2];
+		}
+
+		for (i = 0; i < numofcolor[0]; i++)
+			nums[i] = 0;
+		for (; i < numofcolor[0] + numofcolor[1]; i++)
+			nums[i] = 1;
+		for (; i < numofcolor[0] + numofcolor[1] + numofcolor[2]; i++)
+			nums[i] = 2;
+	}
+};
+
+int main()
+{
+	return 0;
+}
