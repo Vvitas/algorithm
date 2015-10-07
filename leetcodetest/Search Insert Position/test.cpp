d264ce61277a7ec68a75480605c415e41f8d701b
jxd134 <jinxd134@foxmail.com>
2015-10-07 15:58:37 +0800

    Search Insert Position solution.

---
+++ leetcodetest/Search Insert Position/test.cpp
@@ -0,0 +1,27 @@
+#include"../leetcodetest/head.h"
+
+class Solution
+{
+public:
+	int searchInsert(vector<int>& nums, int target)
+	{
+		int left = 0, right = nums.size() - 1, mid, length = nums.size();
+
+		while (left <= right)
+		{
+			mid = left + (right - left) / 2;
+
+			if (target == nums[mid])
+				return mid;
+			else if (target < nums[mid])
+				right = mid - 1;
+			else left = mid + 1;
+		}
+		return left;
+	}
+};
+
+int main()
+{
+	return 0;
+}
