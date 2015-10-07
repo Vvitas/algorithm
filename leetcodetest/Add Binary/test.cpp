d5581c9ab6767b7d1d8f95ea91f1138450fa975d
jxd134 <jinxd134@foxmail.com>
2015-10-07 15:09:43 +0800

    modify add binary solution

---
+++ leetcodetest/Add Binary/test.cpp
@@ -0,0 +1,74 @@
+#include"../leetcodetest/head.h"
+
+class Solution
+{
+public:
+	string addBinary(string a, string b)
+	{
+		if (a.size() < b.size())
+		{
+			string temp = a;a = b;b = temp;
+		}
+
+		bool sign = false;
+		int i, len1 = a.size() - 1, len2 = b.size() - 1;
+		for (i = 0; i < b.size(); i++)
+		{
+			if (a[len1 - i] == '0' && b[len2 - i] == '0')
+			{
+				if (sign) //sign=false
+				{
+					a[len1 - i] = '1'; sign = false;
+				}
+			}
+			else if ((a[len1 - i] == '0' && b[len2 - i] == '1') || (a[len1 - i] == '1' && b[len2 - i] == '0'))
+			{
+				if (sign) //sign=true
+				{
+					a[len1 - i] = '0';
+				}
+				else //sign=false
+				{
+					a[len1 - i] = '1';
+				}
+			}
+			else
+			{
+				if (sign) //sign=true
+				{
+					a[len1 - i] = '1';
+				}
+				else //sign=true
+				{
+					a[len1 - i] = '0'; sign = true;
+				}
+			}
+		}
+		for (; i < a.size(); i++)
+		{
+			if (a[len1 - i] == '0')
+			{
+				if (sign) //sign=false
+				{
+					a[len1 - i] = '1'; sign = false; break;
+				}
+				else break; //sign=false
+			}
+			else
+			{
+				if (sign) //sign=true
+				{
+					a[len1 - i] = '0';
+				}
+				else break; //sign=false
+			}
+		}
+
+		return sign ? (a = '1' + a) : a;
+	}
+};
+
+int main()
+{
+	return 0;
+}
