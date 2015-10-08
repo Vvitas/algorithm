c185da15814cfa6761266a2a347e9d7d0585e26c
jxd134 <jinxd134@foxmail.com>
2015-10-08 21:19:00 +0800

    minor modify

---
+++ leetcodetest/Unique Paths/test.cpp
@@ -0,0 +1,18 @@
+#include"../leetcodetest/head.h"
+
+class Solution 
+{
+public:
+	int uniquePaths(int m, int n) 
+	{
+		if (m == 1 || n == 1)
+			return 1;
+
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
