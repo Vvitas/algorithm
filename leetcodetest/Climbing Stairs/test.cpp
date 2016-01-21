8850e0be942ba9c4e3b77aeb8ca07cfd02bcb506
jxd134 <jinxd134@foxmail.com>
2016-01-21 10:55:08 +0800

    modify Climbing Stairs solutions

--- leetcodetest/Climbing Stairs/test.cpp
+++ leetcodetest/Climbing Stairs/test.cpp
@@ -5,11 +5,10 @@ class Solution
 public:
 	int climbStairs(int n)
 	{
-		if (n <= 0) return 0;
-		else if (1 == n) return 1;
-		else if (2 == n) return 2;
+		if (n < 0) return 0;
+		else if (n <= 2) return n;
 
-		int i, temp1 = 1, temp2 = 2, result;
+		size_t i, temp1 = 1, temp2 = 2, result;
 		for (i = 2; i < n; i++)
 		{
 			result = temp1 + temp2;
