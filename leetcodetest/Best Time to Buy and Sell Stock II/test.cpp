3d49a560dc861cd8e699362db05e6c9e07569861
jxd134 <jinxd134@foxmail.com>
2016-02-26 16:21:59 +0800

    minor modify in Best Time to Buy and Sell Stock II

--- leetcodetest/Best Time to Buy and Sell Stock II/test.cpp
+++ leetcodetest/Best Time to Buy and Sell Stock II/test.cpp
@@ -1,14 +1,15 @@
 #include"../leetcodetest/head.h"
 
-class Solution 
+class Solution
 {
 public:
-	int maxProfit(vector<int>& prices) 
+	int maxProfit(vector<int>& prices)
 	{
-		int profit = 0;
+		int tmp, profit = 0;
 		for (size_t i = 1; i < prices.size(); i++)
 		{
-			profit += (prices[i] - prices[i - 1]>0) ? (prices[i] - prices[i - 1]) : 0;
+			if ((tmp = prices[i] - prices[i - 1])>0)
+				profit += tmp;
 		}
 		return profit;
 	}
